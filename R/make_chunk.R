f72e7aff06e5e4474c606bb0b108be81ee8f4804
UltanPDoherty <dohertyu@tcd.ie>
2023-01-26 16:51:46 +0000

    Code improvements

--- R/make_chunk.R
+++ R/make_chunk.R
@@ -14,21 +14,20 @@
 #' chunks_of_25 <- c(rep(1, 25), 2:26, rep(27, 25), 28:52, rep(53, 25), 54:78)
 #' make_chunk(iris[, 1:4], chunk_labs = chunks_of_25)
 make_chunk <- function(data, chunk_labs) {
-  data       <- as.matrix(data)
-  chunk_labs <- as.numeric(as.factor(chunk_labs))
+
+  data <- as.matrix(data)
+
   chunk_num  <- length(unique(chunk_labs))
   var_num    <- ncol(data)
-  chunk_mean <- matrix(NA, chunk_num, var_num)
 
-  chunk_tab  <- table(chunk_labs)
-  chunk_size <- as.numeric(chunk_tab)
-  singles    <- is.element(chunk_labs, names(chunk_tab)[chunk_size == 1])
+  chunk_size <- as.numeric(table(chunk_labs))
+  single_chunk <- chunk_size == 1
+  single_obs   <- chunk_size[chunk_labs] == 1
 
-  chunk_mean[chunk_size == 1, ] <- data[singles, ]
-
-  for (l in (1:chunk_num)[chunk_size != 1]) {
-      chunk_mean[l, ] <- colMeans(data[chunk_labs == l, ])
-  }
+  chunk_mean <- matrix(NA, nrow = chunk_num, ncol = var_num)
+  chunk_mean[single_chunk, ] <- data[single_obs, ]
+  chunk_sums <- rowsum(data[!single_obs, ], group = chunk_labs[!single_obs])
+  chunk_mean[!single_chunk, ] <- chunk_sums / chunk_size[!single_chunk]
 
   chunk <- list(labs = chunk_labs,
                 num  = chunk_num,
