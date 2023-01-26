39956213a76501846e661bde937f684979820345
UltanPDoherty <dohertyu@tcd.ie>
2023-01-26 16:51:29 +0000

    Printing & timing changes

--- R/mustlink_em.R
+++ R/mustlink_em.R
@@ -26,20 +26,18 @@
 #' @export
 #'
 #' @examples
-#' iris_frame <- flowCore::flowFrame(exprs = as.matrix(iris[, 1:4]))
 #' iris_tab   <- rbind(se = c(-1, +1, -1, -1),
 #'                     ve = c(00, -1, +1, +1),
 #'                     vi = c(+1, 00, +1, +1))
-#' iris_table_labs <- table_to_label(iris_frame, type_marker = iris_tab)$labs
+#' iris_table_labs <- table_to_label(iris[, 1:4], type_marker = iris_tab)$labs
 #' iris_chunk_labs <- chunklet_cores(iris[, 1:4], table_labs = iris_table_labs)$chunks
 #' mustlink_em(iris[, 1:4], clust_num = 3, chunk_labs = iris_chunk_labs)
 mustlink_em <- function(data, clust_num, chunk_labs,
                         maxit = 100, eps = 1e-10, start = "k-Means",
-                        init_seed = NULL) {
-  data <- as.matrix(data)
-  chunk_time <- system.time({
                         init_seed = NULL, print_freq = 10,
                         burnin = 10, no_print = FALSE) {
+
+  setup_time <- system.time({
     if (!is.matrix(data)) {
       if (inherits(data, "flowFrame")) {
         data <- flowCore::exprs(data)
@@ -48,36 +46,27 @@ mustlink_em <- function(data, clust_num, chunk_labs,
       }
     }
     chunk <- make_chunk(data, chunk_labs)
-  })
-  init_time <- system.time({
     params  <- initialise_model(data, clust_num, start, init_seed)
-  })
 
-  cat(paste0("chunk_time: ", chunk_time[3], ",\t",
-             "init_time: ", init_time[3], "\n"
-             )
-      )
+    ll <- c()
+    ll_crit <- NA
+    it <- 0
 
-  ll <- e_time <- m_time <- mid_time <- c()
-  ll_crit <- eps + 1
-  it <- 0
-
-  obs_num <- nrow(data)
-  var_num <- ncol(params$mu)
-
-  burnin <- 10
+    obs_num <- nrow(data)
+    var_num <- ncol(params$mu)
+  })[3]
 
   # EM algorithm
+  if (!no_print) {
+    cat(paste0("...EM started at ", Sys.time(), "\n"))
+  }
+  em_time <- system.time({
     repeat {
       it <- it + 1
 
-    # E-step
-    e_time[it] <- system.time({
       e_out <- mustlink_estep(data, chunk, params,
-                            obs_num, var_num, clust_num)
-    })[3]
+                              obs_num, var_num, clust_num)
 
-    # mid_time[it] <- system.time({
       ll <- append(ll, e_out$ll)
 
       # ll_crit is the relative increase in the log-likelihood.
@@ -94,18 +83,17 @@ mustlink_em <- function(data, clust_num, chunk_labs,
           ll_crit <- ll_diff / abs(ll[it - 1])
         }
       }
-    # })[3]
 
+      if(!no_print & it %% print_freq == 1){
+        cat(paste0("...No. of E-Steps: ", it,
+                   ",\t log-likelihood: ", round(ll[it],     digits = 5),
+                   ",\t Sys.time: ", Sys.time(), "\n"
+                   )
+            )
+      }
 
-    # M-step
-    m_time[it] <- system.time({
-      params <- mustlink_mstep(data, chunk, obs_pp = e_out$obs_pp,
-                               chunk_pp = e_out$chunk_pp,
-                               obs_num, var_num, clust_num)
-      # params <- mustlink_mstep_mclust(data, chunk_num = chunk$num,
-      #                                 obs_pp = e_out$obs_pp,
-      #                                 chunk_pp = e_out$chunk_pp)
-    })[3]
+      # EM has converged if the relative difference between consecutive values
+      # of the log-likelihood, i.e. ll_crit, is not NA and is less than eps.
       if (it == maxit) {
         warning(paste0("EM algorithm did not converge before ", maxit, " iterations."))
         if (!no_print) {
@@ -119,26 +107,30 @@ mustlink_em <- function(data, clust_num, chunk_labs,
         break
       }
 
-    cat(paste0("...EM-", it, ",\t",
-               "e_time: ",   round(e_time[it], digits = 2), ",\t",
-               "ll: ",       round(ll[it],     digits = 5), ",\t",
-               # "mid_time: ", round(mid_time[it], digits = 2), ",\t",
-               "m_time: ",   round(m_time[it], digits = 2), ",\t",
-               "\n"
-               )
-        )
+      params <- mustlink_mstep(data, chunk, obs_pp = e_out$obs_pp,
+                                 chunk_pp = e_out$chunk_pp,
+                                 obs_num, var_num, clust_num)
+      }
+  })[3]
+  if (!no_print) {
+    cat(paste0("...No. of E-Steps: ", it,
+               ",\t log-likelihood: ", round(ll[it],     digits = 5),
+               ",\t EM time: ", round(em_time, digits = 2) , "\n"))
   }
-  # End of EM
 
   label_time <- system.time({
     chunk_to_clust <- apply(X = e_out$chunk_pp, MARGIN = 1, FUN = which.max)
     clust_labs     <- chunk_to_clust[chunk$labs]
+  })[3]
+
+  times <- c(setup_time, it, em_time, label_time)
+  names(times) <- c("setup", "iterations", "em", "label")
 
   res <- list(clust_labs = clust_labs,
               pp = e_out$chunk_pp,
               params = params,
               ll = ll,
-              time = cbind(e_time, mid_time, m_time, e_time + mid_time + m_time)
+              times = times
               )
 
   return(res)
