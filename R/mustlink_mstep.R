9e228df1fcd6338a10989728dc3d8262a6ce154b
UltanPDoherty <dohertyu@tcd.ie>
2023-01-31 18:47:31 +0000

    Swapped chunk arg. for chunk_num

--- R/mustlink_mstep.R
+++ R/mustlink_mstep.R
@@ -4,12 +4,12 @@
 #' Implement M-step of EM algorithm for GMM with positive / must-link constraints.
 #'
 #' @param data Dataset being clustered in matrix form.
-#' @param chunk Object from make_chunk.
 #' @param obs_pp Expanded observation posterior probability matrix.
 #' @param chunk_pp Chunklet posterior probability matrix.
-#' @param obs_num Number of observations in the dataset.
-#' @param var_num Number of varibles in the dataset.
+#' @param chunk_num Number of chunklets.
 #' @param clust_num Number of clusters pre-specified.
+#' @param obs_num Number of observations in the dataset.
+#' @param var_num Number of variables in the dataset.
 #'
 #' @return List containing prop, mu, sigma.
 #' @export
@@ -23,16 +23,16 @@
 #'                           obs_num = 150, var_num = 4, clust_num = 3)
 #' obs_pp1 <- e_out1$obs_pp
 #' chunk_pp1 <- e_out1$chunk_pp
-#' mustlink_mstep(as.matrix(iris[, 1:4]), chunk = chunk1,
-#'                obs_pp = obs_pp1, chunk_pp = chunk_pp1,
-#'                obs_num = 150, var_num = 4, clust_num = 3)
-mustlink_mstep <- function(data, chunk, obs_pp, chunk_pp,
+#' mustlink_mstep(as.matrix(iris[, 1:4]),
+#'                obs_pp = obs_pp1, chunk_pp = chunk_pp1)
+mustlink_mstep <- function(data, obs_pp, chunk_pp,
+                           chunk_num = nrow(chunk_pp),
+                           clust_num = ncol(chunk_pp),
                            obs_num = nrow(data),
-                           var_num = ncol(data),
-                           clust_num = ncol(chunk_pp)) {
+                           var_num = ncol(data)) {
 
   # Chunklet mixing proportions
-  prop <- colSums(chunk_pp) / chunk$num
+  prop <- colSums(chunk_pp) / chunk_num
 
   obs_pp_sums <- colSums(obs_pp)
   obs_pp2 <- sweep(x = obs_pp, MARGIN = 2, STATS = obs_pp_sums, FUN = "/")
