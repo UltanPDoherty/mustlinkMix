157ade431ab02d2605598e8d15fdffb2e174714d
UltanPDoherty <72346121+UltanPDoherty@users.noreply.github.com>
2023-04-01 15:01:09 +0100

    chunk_to_core: similpified code.

--- R/chunk_to_core.R
+++ R/chunk_to_core.R
@@ -1,18 +1,25 @@
-#' Convert chunk_labels to core_labels, i.e. set all singleton chunklet labels to 0.
+#' @title Convert chunk_labels to core_labels.
+#'
+#' @description
+#' Instead of giving each unconstrained event its own chunklet label, label all
+#' unconstrained event as belonging to chunklet 0.
 #'
 #' @param chunk_labels Integer vector of chunklet labels.
 #'
 #' @return Integer vector of core_labels, same length as chunk_labels.
 #' @export
+
 chunk_to_core <- function(chunk_labels) {
 
-  obs_num <- length(chunk_labels)
-
+  # Table counting number of events with each chunklet label.
   chunk_tab <- table(chunk_labels)
 
-  core_labels <- vector("integer", length = obs_num)
-  core_labels[chunk_tab[chunk_labels] > 1] <- chunk_labels[chunk_tab[chunk_labels] > 1]
-  core_labels[chunk_tab[chunk_labels] == 1] <- 0
+  # Logical vector indicating unconstrained events.
+  unconstrained <- chunk_tab[chunk_labels] == 1
+
+  # Relabel all unconstrained event as 0.
+  core_labels <- chunk_labels
+  core_labels[unconstrained] <- 0
 
   return(core_labels)
 }
