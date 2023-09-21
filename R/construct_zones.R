f9358e35be6113c14618438eb55a73d722c9e464
UltanPDoherty <72346121+UltanPDoherty@users.noreply.github.com>
2023-09-21 11:32:37 -0400

    construct_zones: remove upper and lower splits

--- R/construct_zones.R
+++ R/construct_zones.R
@@ -29,16 +29,10 @@ construct_zones <- function(data, type_marker, custom_splits = NULL) {
   if (is.null(custom_splits)) {
     splits <- compute_splits(data, type_marker = type_marker)
   } else {
-    splits <- data.frame(lower = custom_splits,
-                         upper = custom_splits)
+    splits <- custom_splits
   }
 
   # create a +1/-1 matrix the same size as the data flowFrame
-  is_event_positive <- t(apply(X = data, MARGIN = 1,
-                               FUN = function(event) event > splits$upper))
-  is_event_negative <- t(apply(X = data, MARGIN = 1,
-                               FUN = function(event) event < splits$lower))
-  event_plusminus <- is_event_positive - is_event_negative
 
   zone_matrix <- matrix(NA, nrow = event_num, ncol = zone_num,
                         dimnames = list(NULL, zone_names))
@@ -111,31 +105,6 @@ compute_splits <- function(data, type_marker) {
   to_be_split <- vapply(nonzero_markers, FUN.VALUE = logical(1), FUN = any)
 
   # create a vector of +/- splits to check every row of the data against
-  splits <- data.frame(upper = rep(NA, var_num),
-                       default = rep(NA, var_num),
-                       lower = rep(NA, var_num))
-  splits$upper[to_be_split] <- vapply(which(to_be_split), FUN.VALUE = double(1),
-                                      FUN = function(p) {
-                                        flowDensity::deGate(data[, p],
-                                                            upper = TRUE,
-                                                            verbose = FALSE)
-                                })
-  splits$default[to_be_split] <- vapply(which(to_be_split), FUN.VALUE = double(1),
-                                      FUN = function(p) {
-                                        flowDensity::deGate(data[, p],
-                                                            upper = NA,
-                                                            verbose = FALSE)
-                                      })
-  splits$lower[to_be_split] <- vapply(which(to_be_split), FUN.VALUE = double(1),
-                                      FUN = function(p) {
-                                        flowDensity::deGate(data[, p],
-                                                            upper = FALSE,
-                                                            verbose = FALSE)
-                                      })
-  for (i in 1:var_num) {
-    if (to_be_split[i] & !all(splits[i, 1] == splits[i, ])) {
-      message(paste0("Upper & lower flowDensity splits used for marker ", i,
-                     ": ", colnames(data)[i], ".\n"))
     }
   }
 
