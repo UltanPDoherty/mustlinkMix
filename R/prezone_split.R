ae7b15debddc63fd436a791e5323d7db104775d4
UltanPDoherty <72346121+UltanPDoherty@users.noreply.github.com>
2023-09-22 14:08:34 -0400

    Creating prezone_splits.R

--- R/prezone_split.R
+++ R/prezone_split.R
@@ -28,70 +28,89 @@ weight_prezones <- function(pz_labels) {
 
   pz_weights <- 1 / (pz_num * pz_sizes[pz_labels])
 
-  for (i in seq_along(pz_weights)) {
-    if (pz_weights[i] > 1 / 1000) {
-      pz_weights[i] <- 1 / 1000
-    }
+  while (max(pz_weights) > 1 / 1000) {
+    pz_weights[pz_weights > 1 / 1000] <- 1 / 1000
+    pz_weights <- pz_weights / sum(pz_weights)
   }
 
-  pz_weights / sum(pz_weights)
+  return(pz_weights)
 }
 
-find_valley <- function(dens, width_percent = 0.01, peak_quantile = 0.75) {
-  is_peak <- peak_left <- peak_right <- c()
+find_peaks <- function(dens, width_percent = 0.01, height_percent = 0.01) {
+
+  # w is the number of density points in width_percent of the range
   w <- round(length(dens$x) * width_percent)
+
+  is_peak <- peak_left <- peak_right <- c()
   for (i in seq(w + 1, length(dens$y) - w)) {
     peak_left[i - w] <- all(dens$y[i] > dens$y[(i - w - 1):(i - 1)])
     peak_right[i - w] <- all(dens$y[i] > dens$y[(i + 1):(i+w)])
     is_peak[i - w] <- peak_left[i - w] & peak_right[i - w]
   }
 
-  not_low <- dens$y > quantile(dens$y, peak_quantile)
-  is_big_peak <- c(rep(F, w), is_peak, rep(F, w)) & not_low
+  min_peak <- height_percent * max(dens$y)
+  is_peak <- c(rep(F, w), is_peak, rep(F, w)) & dens$y > min_peak
 
-  if (sum(is_big_peak) == 1) {
-    message("Only one peak found.")
-    return(NA)
-  }
+  return(is_peak)
+}
 
-  peak1 <- data.frame(x = dens$x[which.max(dens$y)],
-                      y = max(dens$y))
-  peak2 <- data.frame(x = dens$x[is_big_peak & dens$y != max(dens$y)],
-                      y = dens$y[is_big_peak & dens$y != max(dens$y)])
+find_valley <- function(dens, is_peak) {
+  peak1_ind <- which.max(dens$y)
+  peak1 <- data.frame(x = dens$x[peak1_ind],
+                      y = dens$y[peak1_ind])
+  peak2_ind <- which(is_peak & dens$y != max(dens$y))
+  peak2 <- data.frame(x = dens$x[peak2_ind],
+                      y = dens$y[peak2_ind])
 
-  scores <- valleys <- c()
+  scores <- valleys <- valley_ind <- c()
+  interpeak <- matrix(nrow = length(dens$x), ncol = length(peak2$x))
   for (i in seq_along(peak2$x)) {
     if (peak2$x[i] < peak1$x) {
-      valleys[i] <- min(dens$y[dens$x > peak2$x[i] & dens$x < peak1$x])
+      interpeak[, i] <- dens$x > peak2$x[i] & dens$x < peak1$x
+      valley_ind[i] <- peak2_ind[i] + which.min(dens$y[interpeak[, i]])
+      valleys[i] <- dens$y[valley_ind[i]]
     } else {
-      valleys[i] <- min(dens$y[dens$x > peak1$x & dens$x < peak2$x[i]])
+      interpeak[, i] <- dens$x > peak1$x & dens$x < peak2$x[i]
+      valley_ind[i] <- peak1_ind + which.min(dens$y[interpeak[, i]])
+      valleys[i] <- dens$y[valley_ind[i]]
     }
     scores[i] <- peak2$y[i] - valleys[i]
   }
 
-  return(dens$x[dens$y == valleys[which.max(scores)]])
+  return(dens$x[valley_ind[which.max(scores)]])
 }
 
-prezone_split <- function(data, splits, type_marker,
-                          width_percent = 0.01, peak_quantile = 0.75) {
+advanced_split <- function(data, type_marker, splits,
+                          width_percent = 0.01) {
+  var_num <- ncol(data)
 
-  pz_labels <- label_prezones(data, splits)
-  pz_weights <- weight_prezones(pz_labels)
-
-  nonzero_markers <- lapply(seq_len(ncol(type_marker)),
+  nonzero_markers <- lapply(seq_len(var_num),
                             FUN = function(j) type_marker[, j] != 0)
   to_be_split <- vapply(nonzero_markers, FUN.VALUE = logical(1), FUN = any)
 
-  pz_density <- list()
+  if (!is.null(splits)) {
+    pz_labels <- label_prezones(data, splits)
+    pz_weights <- weight_prezones(pz_labels)
+  } else {
+    pz_weights <- NULL
+    splits <- rep(NA, var_num)
+  }
+
+  pz_density <- pz_peaks <- list()
   pz_splits <- splits
 
   for (j in seq_along(splits)) {
     if (is.na(pz_splits[j]) & to_be_split[j]) {
-      cat("prezone_split for marker ", j, "\n")
+      cat("advanced_split for marker ", j, "\n")
       pz_density[[j]] <- density(data[, j], weights = pz_weights,
                                  warnWbw = FALSE)
-      pz_splits[j] <- find_valley(pz_density[[j]],
-                                  width_percent, peak_quantile)
+      pz_peaks[[j]] <- find_peaks(pz_density[[j]], width_percent)
+      if (sum(pz_peaks[[j]]) == 1) {
+        message("Only one peak found.")
+        pz_splits[j] <- NA
+      } else {
+        pz_splits[j] <- find_valley(pz_density[[j]], pz_peaks[[j]])
+      }
     }
   }
 
