6d94c3592f55e6041f3f5b0b27e21e3cac75352c
UltanPDoherty <72346121+UltanPDoherty@users.noreply.github.com>
2023-12-18 16:14:39 +0000

    find_peaks: height_percent to min_height

--- R/prezone_split.R
+++ R/prezone_split.R
@@ -36,7 +36,25 @@ weight_prezones <- function(pz_labels) {
   return(pz_weights)
 }
 
-find_peaks <- function(dens, width_percent = 0.01, height_percent = 0.01) {
+# find_peaks <- function(dens, width_percent = 0.01, height_percent = 0) {
+#
+#   # w is the number of density points in width_percent of the range
+#   w <- round(length(dens$x) * width_percent)
+#
+#   is_peak <- peak_left <- peak_right <- c()
+#   for (i in seq(w + 1, length(dens$y) - w)) {
+#     peak_left[i - w] <- all(dens$y[i] > dens$y[(i - w - 1):(i - 1)])
+#     peak_right[i - w] <- all(dens$y[i] > dens$y[(i + 1):(i+w)])
+#     is_peak[i - w] <- peak_left[i - w] & peak_right[i - w]
+#   }
+#
+#   min_peak <- height_percent * max(dens$y)
+#   is_peak <- c(rep(F, w), is_peak, rep(F, w)) & dens$y > min_peak
+#
+#   return(is_peak)
+# }
+
+find_peaks <- function(dens, width_percent = 0.01, min_height = 0.01) {
 
   # w is the number of density points in width_percent of the range
   w <- round(length(dens$x) * width_percent)
@@ -48,8 +66,7 @@ find_peaks <- function(dens, width_percent = 0.01, height_percent = 0.01) {
     is_peak[i - w] <- peak_left[i - w] & peak_right[i - w]
   }
 
-  min_peak <- height_percent * max(dens$y)
-  is_peak <- c(rep(F, w), is_peak, rep(F, w)) & dens$y > min_peak
+  is_peak <- c(rep(F, w), is_peak, rep(F, w)) & dens$y > min_height
 
   return(is_peak)
 }
