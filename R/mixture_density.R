92e750346748c00baaae3a306e9d973ae7630817
UltanPDoherty <72346121+UltanPDoherty@users.noreply.github.com>
2024-08-06 18:55:57 +0100

    mixture_density

---
+++ R/mixture_density.R
@@ -0,0 +1,17 @@
+mixture_density <- function(data, params) {
+  clust_num <- length(params$prop)
+  obs_num <- nrow(data)
+
+  dens_mat <- matrix(nrow = obs_num, ncol = clust_num)
+  for (k in seq_len(clust_num)) {
+    dens_mat[, k] <- mvtnorm::dmvnorm(
+      data,
+      mean = params$mu[k, ],
+      sigma = params$sigma[, , k]
+    )
+  }
+
+  dens_vec <- params$prop %*% dens_mat
+
+  return(dens_vec)
+}
